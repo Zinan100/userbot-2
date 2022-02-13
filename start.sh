50e3c128a1182166f1f1c2afea0cec1eae46f4bf
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-13 16:04:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/Jackandoggy/0.git /0
+cd /0
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
