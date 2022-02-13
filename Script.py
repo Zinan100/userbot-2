cc48d3d59b4a98a8ede6ab0d9813e659bd574bc0
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-13 13:57:33 +0530

    Update and rename script.py to Script.py

---
+++ Script.py
@@ -0,0 +1,505 @@
+class script(object):
+    START_TXT = """𝖧𝖾𝗅𝗅𝗈 {},
+
+𝙸 𝚊𝚖 𝚊 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚖 𝚋𝚘𝚝 𝚍𝚎𝚜𝚒𝚐𝚗𝚎𝚍 𝚎𝚜𝚙𝚎𝚌𝚒𝚊𝚕𝚕𝚢 𝚝𝚘 𝚖𝚊𝚗𝚊𝚐𝚎 𝚖𝚘𝚟𝚒𝚎 𝚐𝚛𝚘𝚞𝚙𝚜.
+𝙰𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚜 𝚊𝚗 𝙰𝚍𝚖𝚒𝚗 𝚊𝚗𝚍 𝚎𝚗𝚓𝚘𝚢 𝚕𝚒𝚖𝚒𝚝𝚕𝚎𝚜𝚜 𝚖𝚘𝚟𝚒𝚎𝚜 ✨.
+
+𝖢𝗅𝗂𝖼𝗄 𝗆𝗒 <b>𝖧𝖾𝗅𝗉</b> 𝖬𝗈𝖽𝗎𝗅𝖾 𝗍𝗈 𝖲𝖾𝖾 𝗆𝗒 𝖥𝗎𝗇𝖼𝗍𝗂𝗈𝗇𝗌 𝖺𝗇𝖽 𝗍𝗈 𝖪𝗇𝗈𝗐 𝖧𝗈𝗐 𝗍𝗈 𝗎𝗌𝖾 𝗆𝖾"""
+    HELP_TXT = """𝖶𝖺𝗌𝗌𝗎𝗉 {},
+
+/about: 𝖳𝗈 𝗄𝗇𝗈𝗐 𝗆𝗒 𝖽𝖾𝗍𝖺𝗂𝗅𝗌
+
+𝚃𝙰𝙿 𝙾𝙽 𝙴𝙰𝙲𝙷 𝙼𝙾𝙳𝚄𝙻𝙴 𝚃𝙾 𝙶𝙴𝚃 𝚃𝙷𝙴 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 ✨."""
+    HELPTWO_TXT = """𝖧𝖾𝗒 {},
+
+/about: 𝖳𝗈 𝗄𝗇𝗈𝗐 𝗆𝗒 𝖽𝖾𝗍𝖺𝗂𝗅𝗌
+
+𝖧𝖾𝗋𝖾 𝗂𝗌 𝗍𝗁𝖾 𝗌𝖾𝖼𝗈𝗇𝖽 𝗉𝖺𝗀𝖾 𝗈𝖿 𝗆𝗒 <b>𝖧𝖾𝗅𝗉</b> 𝖬𝗈𝖽𝗎𝗅𝖾.
+
+𝚃𝙰𝙿 𝙾𝙽 𝙴𝙰𝙲𝙷 𝙼𝙾𝙳𝚄𝙻𝙴 𝚃𝙾 𝙶𝙴𝚃 𝚃𝙷𝙴 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 ✨."""
+    ABOUT_TXT = """
+➢ 𝐌𝐲 𝐍𝐚𝐦𝐞: 𝖯𝗁𝗂𝗅𝗂𝗉 𝖩 𝖢𝗈𝗎𝗅𝗌𝗈𝗇
+    
+➢ 𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐨𝐫: <a href='tg://user?id=1617258761'>🅓︎🅞︎🅝︎ [𝖲𝖿𝗅𝗂𝗑]</a>
+
+➢ 𝐌𝐲 𝐒𝐨𝐮𝐫𝐜𝐞: <a href='https://github.com/EvamariaTG/EvaMaria'>𝖤𝗏𝖺 𝖬𝖺𝗋𝗂𝖺</a>
+
+➢ 𝐌𝐲 𝐋𝐢𝐛𝐫𝐚𝐫𝐲: 𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆
+
+➢ 𝐌𝐲 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: 𝖯𝗒𝗍𝗁𝗈𝗇3
+
+➢ 𝐌𝐲 𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞: 𝖬𝗈𝗇𝗀𝗈 𝖣𝖡 
+
+➢ 𝐌𝐲 𝐒𝐞𝐫𝐯𝐞𝐫: 𝖧𝖾𝗋𝗈𝗄𝗎
+
+➢ 𝐌𝐲 𝐔𝐩𝐝𝐚𝐭𝐞𝐬: <a href='https://t.me/SflixBots'>𝖲𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌</a>
+
+➢ 𝐌𝐲 𝐒𝐮𝐩𝐩𝐨𝐫𝐭: <a href='https://t.me/TameSflix'>𝖲𝖿𝗅𝗂𝗑 𝖲𝗎𝗉𝗉𝗈𝗋𝗍</a>
+
+➤ 𝙸𝚏 𝚢𝚘𝚞 𝚕𝚒𝚔𝚎 𝚝𝚑𝚒𝚜 𝚋𝚘𝚝 𝚙𝚕𝚎𝚊𝚜𝚎 𝚜𝚑𝚊𝚛𝚎 𝚊𝚗𝚍 𝚜𝚞𝚙𝚙𝚘𝚛𝚝 𝚞𝚜 <a href='https://t.me/Phil_Coulson_Sflix_bot?start=DSTORE-MTc5Ml8xNzkzXy0xMDAxNzEyMzE2Mzc0'>⚡</a>"""
+    CORONA_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖢𝗈𝗏𝗂𝖽𝖾
+
+𝚃𝚑𝚒𝚜 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚔𝚗𝚘𝚠 𝚍𝚊𝚢𝚒𝚕 𝚒𝚗𝚏𝚘𝚛𝚖𝚊𝚝𝚒𝚘𝚗 𝚊𝚋𝚘𝚞𝚝 𝚌𝚘𝚟𝚒𝚍𝚎
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ /covid - 𝗎𝗌𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗐𝗂𝗍𝗁 𝗒𝗈𝗎𝗋 𝖼𝗈𝗎𝗇𝗍𝗋𝗒 𝗇𝖺𝗆𝖾 𝗍𝗈 𝗀𝖾𝗍 𝖼𝗈𝗏𝗂𝖽𝖾 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+/covid 𝖨𝗇𝖽𝗂𝖺"""
+    STICKER_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝙄𝙙
+• 𝙐𝙨𝙖𝙜𝙚
+To Get Sticker ID
+  ⭕ 𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+◉ Reply To Any Sticker [/stickerid]"""
+    SHEECH_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙏𝙚𝙭𝙩 𝙏𝙤 𝙎𝙝𝙚𝙚𝙘𝙝
+
+• 𝙐𝙨𝙖𝙜𝙚
+A Module To Convert TEXT To Voice With Language Support
+
+  ⭕ 𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+◉ /tts :- Reply To Any TEXT Message With Language Code To Convert As Audio"""
+    URLSHORT_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖴𝗋𝗅 𝗌𝗁𝗈𝗋𝗍𝗇𝖾𝗋
+
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚜𝚑𝚘𝚛𝚝 𝚊 𝚞𝚛𝚕 
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ /short: 𝗎𝗌𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗐𝗂𝗍𝗁 𝗒𝗈𝗎𝗋 𝗅𝗂𝗇𝗄 𝗍𝗈 𝗀𝖾𝗍 𝗌𝗁𝗈𝗋𝗍𝖾𝖽 𝗅𝗂𝗇𝗄𝗌
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+/short https://t.me/Don_Sflix"""
+    IMAGE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖨𝗆𝖺𝗀𝖾 𝖤𝖽𝗂𝗍𝗈𝗋
+
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚎𝚍𝚒𝚝 𝚒𝚖𝚊𝚐𝚎 𝚟𝚎𝚛𝚢 𝚎𝚊𝚜𝚒𝚕𝚢 
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨"""
+    WHOIS_TXT ="""𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝘼𝙣𝙮 𝙑𝙞𝙙𝙚𝙤 𝙁𝙧𝙤𝙢 𝙔𝙏.
+
+• 𝘜𝘴𝘢𝘨𝘦
+𝘠𝘰𝘶 𝘊𝘢𝘯 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘈𝘯𝘺 𝘝𝘪𝘥𝘦𝘰 𝘍𝘳𝘰𝘮 𝘠𝘰𝘶𝘵𝘶𝘣𝘦
+
+𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+• 𝘛𝘺𝘱𝘦 /video or /mp4 𝘈𝘯𝘥 (𝘝𝘪𝘥𝘦𝘰 Link)
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
+/𝘮𝘱4 https://youtu.be/Your Link"""
+    VIDEO_TXT ="""𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝘼𝙣𝙮 𝙑𝙞𝙙𝙚𝙤 𝙁𝙧𝙤𝙢 𝙔𝙏.
+
+• 𝘜𝘴𝘢𝘨𝘦
+𝘠𝘰𝘶 𝘊𝘢𝘯 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘈𝘯𝘺 𝘝𝘪𝘥𝘦𝘰 𝘍𝘳𝘰𝘮 𝘠𝘰𝘶𝘵𝘶𝘣𝘦
+
+𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+• 𝘛𝘺𝘱𝘦 /video or /mp4 𝘈𝘯𝘥 (𝘝𝘪𝘥𝘦𝘰 Link)
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
+/𝘮𝘱4 https://youtu.be/Your Link"""
+    SONG_TXT ="""𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙎𝙤𝙣𝙜
+
+• 𝘜𝘴𝘢𝘨𝘦
+𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝘼𝙣𝙮 𝙑𝙞𝙙𝙚𝙤 𝙁𝙧𝙤𝙢𝙚 𝙔𝙤𝙪 𝙏𝙪𝙗𝙚 𝙖𝙨 𝘼𝙪𝙙𝙞𝙤 𝙁𝙞𝙡𝙚
+
+𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+• 𝘛𝘺𝘱𝘦 /song or /sf (Song Link)
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
+/song https://youtu.be/Your Link
+
+𝙏𝙝𝙞𝙨 𝙒𝙞𝙡𝙡 𝙃𝙚𝙡𝙥 𝙍𝙞𝙣𝙜𝙩𝙤𝙣𝙚 𝙁𝙖𝙣𝙨 😅
+"""
+    ABOOK_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖠𝗎𝖽𝗂𝗈𝖻𝗈𝗈𝗄
+
+𝚈𝚘𝚞 𝚌𝚊𝚗 𝚌𝚘𝚗𝚟𝚎𝚛𝚝 𝚊 𝙿𝙳𝙵 𝚏𝚒𝚕𝚎 𝚝𝚘 𝚊 𝚊𝚞𝚍𝚒𝚘 𝚏𝚒𝚕𝚎 𝚠𝚒𝚝𝚑 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 ✯
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ /audiobook: 𝖱𝖾𝗉𝗅𝗒 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗍𝗈 𝖺𝗇𝗒 𝖯𝖣𝖥 𝗍𝗈 𝗀𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝗍𝗁𝖾 𝖺𝗎𝖽𝗂𝗈"""
+    JSON_TXT ="""<b>𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙅𝙨𝙤𝙣</b>
+JSON:
+Bot returns json for all replied messages with /json
+
+Features:
+Message Editting JSON
+Pm Support
+Group Support
+
+Note:
+Everyone can use this command , if spaming happens bot will automatically ban you from the group"""
+    PIN_TXT ="""➤ 𝐇𝐞𝐥𝐩: 𝖯𝗂𝗇
+
+𝖸𝗈𝗎 𝖼𝖺𝗇 𝗎𝗌𝖾 𝗍𝗁𝖾𝗌𝖾 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌 𝗍𝗈 𝗉𝗂𝗇 𝖺𝗇𝖽 𝗎𝗇𝗉𝗂𝗇 𝗆𝖾𝗌𝗌𝖺𝗀𝖾𝗌 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/pin - 𝖳𝗈 𝗉𝗂𝗇 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾.
+➪/unpin - 𝖳𝗈 𝗎𝗇𝗉𝗂𝗇 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾.
+➪/unpinall - 𝖳𝗈 𝗎𝗇𝗉𝗂𝗇 𝖺𝗅𝗅 𝗍𝗁𝖾 𝗆𝖾𝗌𝗌𝖺𝗀𝖾𝗌 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 𝖺𝗍 𝗈𝗇𝖼𝖾."""
+    FUN_TXT ="""𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙁𝙪𝙣
+    
+<b>🎲 NOTHING MUCH JUST SOME FUN THINGS</b>
+t𝗋𝗒 𝗍𝗁𝗂𝗌 𝖮𝗎𝗍: 
+𝟣. /dice - Roll The Dice 
+𝟤. /Throw 𝗈𝗋 /Dart - 𝖳𝗈 𝖬𝖺𝗄𝖾 Drat 
+3. /Runs - Jokes 
+4. /Goal or /Shoot - To Make A Goal Or Shoot
+5. /luck - Try Your Luck 🧧"""
+    MANUALFILTER_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖬𝖺𝗇𝗎𝖺𝗅-𝖥𝗂𝗅𝗍𝖾𝗋.
+
+➤ 𝐍𝐨𝐭𝐞:
+1. 𝖬𝖺𝗄𝖾 𝗆𝖾 𝖺𝗇 𝖠𝖽𝗆𝗂𝗇 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉.
+2. 𝖮𝗇𝗅𝗒 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉 𝖠𝖽𝗆𝗂𝗇𝗌 𝖼𝖺𝗇 𝖺𝖽𝖽 𝖿𝗂𝗅𝗍𝖾𝗋𝗌.
+3. 𝖠𝗅𝖾𝗋𝗍 𝖻𝗎𝗍𝗍𝗈𝗇 𝗅𝗂𝗆𝗂𝗍: 64 𝖼𝗁𝖺𝗋𝖺𝖼𝗍𝖾𝗋𝗌.
+
+➤ 𝖬𝖺𝗇𝗎𝖺𝗅-𝖥𝗂𝗅𝗍𝖾𝗋 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌:
+➪/add 𝗈𝗋 /filter - 𝖳𝗈 𝖺𝖽𝖽 𝖺 𝖥𝗂𝗅𝗍𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/filters 𝗈𝗋 /viewfilters - 𝖳𝗈 𝗅𝗂𝗌𝗍 𝖺𝗅𝗅 𝗍𝗁𝖾 𝖿𝗂𝗅𝗍𝖾𝗋𝗌 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/del - 𝖳𝗈 𝖽𝖾𝗅𝖾𝗍𝖾 𝖺 𝗌𝗉𝖾𝖼𝗂𝖿𝗂𝖼 𝖿𝗂𝗅𝗍𝖾𝗋.
+➪/delall - 𝖳𝗈 𝖽𝖾𝗅𝖾𝗍𝖾 𝖺𝗅𝗅 𝗍𝗁𝖾 𝖿𝗂𝗅𝗍𝖾𝗋𝗌 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.[𝖦𝖱𝖮𝖴𝖯 𝖮𝖶𝖭𝖤𝖱𝖲 𝖮𝖭𝖫𝖸]"""
+
+    BUTTON_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖡𝖴𝖳𝖳𝖮𝖭𝖲.
+
+➪ 𝖨 𝗌𝗎𝗉𝗉𝗈𝗋𝗍 𝖻𝗈𝗍𝗁 𝖴𝖱𝖫 & 𝖠𝖫𝖤𝖱𝖳 𝗂𝗇𝗅𝗂𝗇𝖾 𝖻𝗎𝗍𝗍𝗈𝗇𝗌.
+
+➤ 𝐍𝐨𝐭𝐞:
+1. 𝖳𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝗐𝗂𝗅𝗅 𝗇𝗈𝗍 𝖺𝗅𝗅𝗈𝗐𝗌 𝗒𝗈𝗎 𝗍𝗈 𝗌𝖾𝗇𝖽 𝖻𝗎𝗍𝗍𝗈𝗇𝗌 𝗐𝗂𝗍𝗁𝗈𝗎𝗍 𝖺𝗇𝗒 𝖼𝗈𝗇𝗍𝖾𝗇𝗍, 𝗌𝗈 𝖼𝗈𝗇𝗍𝖾𝗇𝗍 𝗂𝗌 𝗆𝖺𝗇𝖽𝖺𝗍𝗈𝗋𝗒.
+2. 𝖨 𝗌𝗎𝗉𝗉𝗈𝗋𝗍 𝖻𝗎𝗍𝗍𝗈𝗇𝗌 𝗐𝗂𝗍𝗁 𝖺𝗇𝗒 𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝗆𝖾𝖽𝗂𝖺 𝗍𝗒𝗉𝖾.
+3. 𝖡𝗎𝗍𝗍𝗈𝗇𝗌 𝗌𝗁𝗈𝗎𝗅𝖽 𝖻𝖾 𝗉𝗋𝗈𝗉𝖾𝗋𝗅𝗒 𝗉𝖺𝗋𝗌𝖾𝖽 𝖺𝗌 𝗆𝖺𝗋𝗄𝖽𝗈𝗐𝗇 𝖿𝗈𝗋𝗆𝖺𝗍.
+
+➤ 𝐔𝐑𝐋 𝐁𝐮𝐭𝐭𝐨𝐧𝐬:
+[𝖡𝗎𝗍𝗍𝗈𝗇 𝖳𝖾𝗑𝗍](buttonurl:𝗁𝗍𝗍𝗉𝗌://𝗍.𝗆𝖾/Sflix2kchats)
+
+➤ 𝐀𝐋𝐄𝐑𝐓 𝐁𝐮𝐭𝐭𝐨𝐧𝐬:
+[𝖡𝗎𝗍𝗍𝗈𝗇 𝖳𝖾𝗑𝗍](buttonalert: Your text)"""
+
+    AUTOFILTER_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐀𝐮𝐭𝐨-𝐅𝐢𝐥𝐭𝐞𝐫.
+
+𝙰𝚞𝚝𝚘𝚏𝚒𝚕𝚝𝚎𝚛 𝚒𝚜 𝚊 𝚏𝚎𝚊𝚝𝚞𝚛𝚎 𝚒𝚗 𝚠𝚑𝚒𝚌𝚑 𝙸 𝚛𝚎𝚜𝚙𝚘𝚗𝚍 𝚝𝚘 𝚝𝚑𝚎 𝚚𝚞𝚎𝚛𝚒𝚎𝚜 𝚒𝚗 𝚝𝚑𝚎 𝚐𝚛𝚘𝚞𝚙 𝚠𝚒𝚝𝚑 𝚝𝚑𝚎 𝙼𝚘𝚟𝚒𝚎/𝚂𝚎𝚛𝚒𝚎𝚜 𝚏𝚒𝚕𝚎𝚜 𝚜𝚝𝚘𝚛𝚎𝚍 𝚒𝚗 𝚖𝚢 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 🌩️.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 & 𝐔𝐬𝐚𝐠𝐞:
+
+➢ /autofilter on/off: 𝖳𝗈 𝗍𝗎𝗋𝗇 𝖮𝖭 𝖺𝗇𝖽 𝗍𝗎𝗋𝗇 𝖮𝖥𝖥 𝗍𝗁𝖾 𝖠𝗎𝗍𝗈𝖿𝗂𝗅𝗍𝖾𝗋 𝖥𝖾𝖺𝗍𝗎𝗋𝖾.
+   ➢ /autofilter: To check the current settings.
+
+➤ 𝐍𝐨𝐭𝐞:
+
+❍ 𝖠𝖽𝖽 𝗆𝖾 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 & 𝗆𝖺𝗄𝖾 𝗆𝖾 𝖺𝗇 𝖠𝖽𝗆𝗂𝗇. 𝖠𝗇𝖽 𝗍𝗎𝗋𝗇 𝗈𝗇 𝖺𝗎𝗍𝗈𝖿𝗂𝗅𝗍𝖾𝗋.
+
+𝖬𝖺𝖽𝖾 𝖻𝗒 @CLaY995"""
+
+    CONNECTION_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖢𝗈𝗇𝗇𝖾𝖼𝗍𝗂𝗈𝗇𝗌.
+
+- 𝖴𝗌𝖾𝖽 𝗍𝗈 𝖼𝗈𝗇𝗇𝖾𝖼𝗍 𝖻𝗈𝗍 𝗍𝗈 𝖯𝖬 𝖿𝗈𝗋 𝗆𝖺𝗇𝖺𝗀𝗂𝗇𝗀 𝖿𝗂𝗅𝗍𝖾𝗋𝗌 
+- 𝗂𝗍 𝗁𝖾𝗅𝗉𝗌 𝗍𝗈 𝖺𝗏𝗈𝗂𝖽 𝗌𝗉𝖺𝗆𝗆𝗂𝗇𝗀 𝗂𝗇 𝗀𝗋𝗈𝗎𝗉𝗌.
+
+➤ 𝐍𝐨𝐭𝐞:
+1. 𝖮𝗇𝗅𝗒 𝖺𝖽𝗆𝗂𝗇𝗌 𝖼𝖺𝗇 𝖺𝖽𝖽 𝖺 𝖼𝗈𝗇𝗇𝖾𝖼𝗍𝗂𝗈𝗇.
+2. 𝖲𝖾𝗇𝖽 /connect 𝖿𝗈𝗋 𝖼𝗈𝗇𝗇𝖾𝖼𝗍𝗂𝗇𝗀 𝗆𝖾 𝗍𝗈 𝗎𝗋 𝖯𝖬.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 & 𝐔𝐬𝐚𝐠𝐞:
+
+➪/connect: 𝖳𝗈 𝖼𝗈𝗇𝗇𝖾𝖼𝗍 𝖺 𝗀𝗋𝗈𝗎𝗉 𝗍𝗈 𝗒𝗈𝗎𝗋 𝖯𝖬.
+➪/disconnect: 𝖳𝗈 𝖽𝗂𝗌𝖼𝗈𝗇𝗇𝖾𝖼𝗍 𝖿𝗋𝗈𝗆 𝖺 𝗀𝗋𝗈𝗎𝗉.
+➪/connections: 𝖳𝗈 𝗅𝗂𝗌𝗍 𝖺𝗅𝗅 𝗒𝗈𝗎𝗋 𝖼𝗈𝗇𝗇𝖾𝖼𝗍𝗂𝗈𝗇𝗌."""
+
+    AUTO_MANUAL_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖥𝗂𝗅𝗍𝖾𝗋𝗌
+
+𝚂𝚎𝚕𝚎𝚌𝚝 𝚝𝚑𝚎 𝚏𝚒𝚕𝚝𝚎𝚛 𝚝𝚢𝚙𝚎 𝚢𝚘𝚞 𝚠𝚊𝚗𝚝 𝚋𝚎𝚕𝚘𝚠💫"""
+    PASTE_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙋𝙖𝙨𝙩𝙚
+
+Paste some texts or documents on a website!
+
+<b>Commands and Usage:</b>
+• /paste [text] - paste the given text on Pasty
+• /paste [reply] - paste the replied text on Pasty
+
+<b>NOTE:</b>
+• Bot should have admin privillage.
+• These commands works on both pm and group.
+• These commands can be used by any group member."""
+
+    TGRAPH_TXT = """➤ 𝐇𝐞𝐥𝐩: Telegraph.
+
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚐𝚎𝚗𝚎𝚛𝚊𝚝𝚎 𝚊 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚙𝚑 𝚕𝚒𝚗𝚔 𝚘𝚗 𝚊𝚗𝚢 𝚖𝚎𝚍𝚒𝚊 𝚋𝚎𝚕𝚘𝚠 5𝙼𝙱.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/telegraph: 𝖱𝖾𝗉𝗅𝗒 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗍𝗈 𝖺𝗇𝗒 𝗆𝖾𝖽𝗂𝖺 𝗍𝗈 𝗀𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝗍𝗁𝖾 𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗉𝗁 𝗅𝗂𝗇𝗄."""
+
+    INFO_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖨𝗇𝖿𝗈
+
+𝚆𝚒𝚝𝚑 𝚝𝚑𝚎𝚜𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝙸 𝚌𝚊𝚗 𝚏𝚎𝚝𝚌𝚑 𝚝𝚑𝚎 𝚞𝚜𝚎𝚛 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚜𝚞𝚌𝚑 𝚊𝚜: 𝚄𝚜𝚎𝚛 𝙸𝙳, 𝚄𝚜𝚎𝚛 𝙳𝙿, 𝚄𝚜𝚎𝚛 𝙳𝙲, 𝚄𝚜𝚎𝚛 𝙽𝚊𝚖𝚎𝚜, 𝚎𝚝𝚌. 𝙰𝚕𝚜𝚘 𝚞𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚝𝚘 𝚏𝚎𝚝𝚌𝚑 𝚐𝚛𝚘𝚞𝚙 𝙸𝙳'𝚜 👤.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/id: 𝖳𝗈 𝗀𝖾𝗍 𝗍𝗁𝖾 𝖨𝖣 𝗈𝖿 𝖺 𝗎𝗌𝖾𝗋.
+➪/info: 𝖳𝗈 𝗀𝖾𝗍 𝗂𝗇𝖿𝗈 𝖺𝖻𝗈𝗎𝗍 𝖺 𝗎𝗌𝖾𝗋."""
+
+    GTRANS_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖦𝗈𝗈𝗀𝗅𝖾 𝖳𝗋𝖺𝗇𝗌𝗅𝖺𝗍𝖾𝗋
+
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚝𝚛𝚊𝚗𝚜𝚕𝚊𝚝𝚎 𝚊 𝚝𝚎𝚡𝚝 𝚝𝚘 𝖺𝗇𝗒 𝚕𝚊𝚗𝚐𝚞𝚊𝚐𝚎𝚜 𝚢𝚘𝚞 𝚠𝚊𝚗𝚝. 𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚠𝚘𝚛𝚔𝚜 𝚘𝚗 𝚋𝚘𝚝𝚑 𝚙𝚖 𝚊𝚗𝚍 𝚐𝚛𝚘𝚞𝚙 ✯
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/tr - 𝖳𝗈 𝗍𝗋𝖺𝗇𝗌𝗅𝖺𝗍𝖾𝗋 𝗍𝖾𝗑𝗍𝗌 𝗍𝗈 𝖺 𝗌𝗉𝖾𝖼𝗂𝖿𝖼 𝗅𝖺𝗇𝗀𝗎𝖺𝗀𝖾
+
+➤ 𝖭𝗈𝗍𝖾:
+𝖶𝗁𝗂𝗅𝖾 𝗎𝗌𝗂𝗇𝗀 /tr 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗉𝖾𝖼𝗂𝖿𝗒 𝗍𝗁𝖾 𝗅𝖺𝗇𝗀𝗎𝖺𝗀𝖾 𝖼𝗈𝖽𝖾
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾: /𝗍𝗋 𝗆𝗅
+• 𝖾𝗇 = 𝖤𝗇𝗀𝗅𝗂𝗌𝗁
+• 𝗆𝗅 = 𝖬𝖺𝗅𝖺𝗒𝖺𝗅𝖺𝗆
+• 𝗁𝗂 = 𝖧𝗂𝗇𝖽𝗂"""
+
+    SEARCH_TXT = """➤ 𝐇𝐞𝐥𝐩: IMDB.
+
+𝚆𝚒𝚝𝚑 𝚝𝚑𝚒𝚜 𝙼𝚘𝚍𝚞𝚕𝚎 𝙸 𝚌𝚊𝚗 𝚏𝚎𝚝𝚌𝚑 𝙼𝚘𝚟𝚒𝚎/𝚂𝚎𝚛𝚒𝚎𝚜 𝚍𝚎𝚝𝚊𝚒𝚕𝚜 𝚏𝚛𝚘𝚖 𝙸𝙼𝙳𝚋. 𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚒𝚜 𝚊𝚕𝚜𝚘 𝙰𝚙𝚙𝚕𝚒𝚌𝚊𝚋𝚕𝚎 𝚒𝚗 𝙲𝚑𝚊𝚗𝚗𝚎𝚕𝚜. 𝙹𝚞𝚜𝚝 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚗 𝙰𝚍𝚖𝚒𝚗 𝚒𝚗 𝚢𝚘𝚞𝚛 𝚌𝚑𝚊𝚗𝚗𝚎𝚕 𝚊𝚗𝚍 𝙶𝚒𝚟𝚎 𝚞𝚜𝚎 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 ✨.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/imdb: 𝖳𝗈 𝗀𝖾𝗍 𝖺 𝖿𝗂𝗅𝗆 𝗂𝗇𝖿𝗈 𝖿𝗋𝗈𝗆 𝖨𝖬𝖣𝖻.
+➪/search: 𝖳𝗈 𝗀𝖾𝗍 𝖿𝗂𝗅𝗆 𝗂𝗇𝖿𝗈 𝖿𝗋𝗈𝗆 𝗏𝖺𝗋𝗂𝗈𝗎𝗌 𝗌𝗈𝗎𝗋𝖼𝖾𝗌."""
+
+    PURGE_TXT = """➤ 𝐇𝐞𝐥𝐩: Purge.
+
+𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚒𝚏 𝚢𝚘𝚞 𝚠𝚊𝚗𝚝 𝚝𝚘 𝚍𝚎𝚕𝚎𝚝𝚎 𝚊 𝚋𝚞𝚕𝚔 𝚘𝚏 𝚖𝚎𝚜𝚜𝚊𝚐𝚎𝚜 𝚏𝚛𝚘𝚖 𝚢𝚘𝚞𝚛 𝚌𝚑𝚊𝚝 𝚐𝚛𝚘𝚞𝚙.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪/purge - 𝖳𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝖽𝖾𝗅𝖾𝗍𝖾𝗌 𝖺𝗅𝗅 𝗍𝗁𝖾 𝗆𝖾𝗌𝗌𝖺𝗀𝖾𝗌 𝗎𝗉𝗍𝗈 𝗍𝗁𝖾 𝗋𝖾𝗉𝗅𝗂𝖾𝖽 𝗆𝖾𝗌𝗌𝖺𝗀𝖾."""
+
+    RESTRIC_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖱𝖾𝗌𝗍𝗋𝗂𝖼𝗍𝗂𝗈𝗇𝗌
+
+𝚃𝚑𝚎𝚜𝚎 𝚊𝚛𝚎 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊 𝚐𝚛𝚘𝚞𝚙 𝚊𝚍𝚖𝚒𝚗 𝚌𝚊𝚗 𝚞𝚜𝚎 𝚝𝚘 𝚖𝚊𝚗𝚊𝚐𝚎 𝚝𝚑𝚎𝚒𝚛 𝚐𝚛𝚘𝚞𝚙 𝚖𝚘𝚛𝚎 𝚎𝚏𝚏𝚒𝚌𝚒𝚎𝚗𝚝𝚕𝚢.
+
+➪/ban: 𝖳𝗈 𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋 𝖿𝗋𝗈𝗆 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/unban: 𝖳𝗈 𝗎𝗇𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/tban: 𝖳𝗈 𝗍𝖾𝗆𝗉𝗈𝗋𝖺𝗋𝗂𝗅𝗒 𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋.
+➪/mute: 𝖳𝗈 𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/unmute: 𝖳𝗈 𝗎𝗇𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/tmute: 𝖳𝗈 𝗍𝖾𝗆𝗉𝗈𝗋𝖺𝗋𝗂𝗅𝗒 𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋.
+
+➤ 𝖭𝗈𝗍𝖾:
+𝖶𝗁𝗂𝗅𝖾 𝗎𝗌𝗂𝗇𝗀 /tmute 𝗈𝗋 /tban 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗉𝖾𝖼𝗂𝖿𝗒 𝗍𝗁𝖾 𝗍𝗂𝗆𝖾 𝗅𝗂𝗆𝗂𝗍.
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾: /𝗍𝖻𝖺𝗇 2𝖽 𝗈𝗋 /𝗍𝗆𝗎𝗍𝖾 2𝖽.
+𝖸𝗈𝗎 𝖼𝖺𝗇 𝗎𝗌𝖾 𝗏𝖺𝗅𝗎𝖾𝗌: 𝗆/𝗁/𝖽. 
+ • 𝗆 = 𝗆𝗂𝗇𝗎𝗍𝖾𝗌
+ • 𝗁 = 𝗁𝗈𝗎𝗋𝗌
+ • 𝖽 = 𝖽𝖺𝗒𝗌"""
+
+    ADMIN_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖠𝖽𝗆𝗂𝗇𝗌
+
+𝚃𝚑𝚎𝚜𝚎 𝚊𝚛𝚎 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊 𝚐𝚛𝚘𝚞𝚙 𝚊𝚍𝚖𝚒𝚗 𝚌𝚊𝚗 𝚞𝚜𝚎 𝚝𝚘 𝚌𝚞𝚜𝚝𝚘𝚖𝚒𝚜𝚎 𝚝𝚑𝚎 𝚋𝚘𝚝 𝚊𝚜 𝚝𝚑𝚎𝚒𝚛 𝚠𝚒𝚜𝚑.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ /set_template: 𝖳𝗒𝗉𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗐𝗂𝗍𝗁 𝗒𝗈𝗎𝗋 𝗇𝖾𝗐 𝖳𝖾𝗆𝗉𝗅𝖺𝗍𝖾 𝗍𝗈 𝗌𝖾𝗍 𝗇𝖾𝗐 𝖼𝗎𝗌𝗍𝗈𝗆𝗂𝗌𝖾𝖽 𝖨𝖬𝖣𝖡 𝖳𝖾𝗆𝗉𝗅𝖺𝗍𝖾.
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+/set_template
+<b>🏷 Title</b>: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>"""
+
+    STATUS_TXT = """➪ <b>Total Files:</b> <code>{}</code>
+
+➪ <b>Total Users:</b> <code>{}</code>
+
+➪ <b>Total Chats:</b> <code>{}</code>
+
+➪ <b>Used Storage:</b> <code>{}</code> 
+
+➪ <b>Free Storage:</b> <code>{}</code> """
+
+    TTS_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙏𝙚𝙭𝙩 𝙏𝙤 𝙑𝙤𝙞𝙘𝙚
+A module to convert text to voice with language support.
+
+Commands and Usage:
+• /tts - Reply to any text message to convert as audio."""
+
+    FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖥𝗂𝗅𝖾 𝖲𝗍𝗈𝗋𝖾
+
+𝚆𝚒𝚝𝚑 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚍𝚜 𝚒 𝚌𝚊𝚗 𝚜𝚝𝚘𝚛𝚎 𝚏𝚒𝚕𝚎𝚜 𝚊𝚗𝚍 𝚐𝚒𝚟𝚎 𝚢𝚘𝚞 𝚊 𝚜𝚑𝚊𝚛𝚎𝚊𝚋𝚕𝚎 𝚕𝚒𝚗𝚔 𝚠𝚒𝚝𝚑 𝚝𝚑𝚒𝚜 𝚕𝚒𝚗𝚔 𝚒 𝚌𝚊𝚗 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚊𝚝 𝚏𝚒𝚕𝚎𝚜 𝚢𝚘𝚞 𝚐𝚒𝚟𝚎 𝚖𝚎 𝚏𝚛𝚘𝚖 𝚊𝚗𝚢 𝚌𝚑𝚊𝚗𝚗𝚎𝚕 𝚠𝚒𝚝𝚑𝚘𝚞𝚝 𝚊𝚍𝚍𝚒𝚗𝚐 𝚖𝚎
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➪ /plink - 𝖱𝖾𝗉𝗅𝗒 𝗍𝗈 𝖺𝗇𝗒 𝗆𝖾𝖽𝗂𝖺 𝗍𝗈 𝗀𝖾𝗍 𝗅𝗂𝗇𝗄
+➪ /pbatch - 𝖴𝗌𝖾 𝗒𝗈𝗎𝗋 𝗆𝖺𝖽𝗂𝖺 𝗅𝗂𝗇𝗄 𝗐𝗂𝗍𝗁 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽
+
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+/pbatch <code>https://t.me/Sflix2k/497 https://t.me/Sflix2k/498</code>"""
+
+    REPORT_TXT = """➤ 𝐇𝐞𝐥𝐩: Reports.
+
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚛𝚎𝚙𝚘𝚛𝚝 𝚊 𝚖𝚎𝚜𝚜𝚊𝚐𝚎 𝚘𝚛 𝚊 𝚞𝚜𝚎𝚛 𝚝𝚘 𝚝𝚑𝚎 𝚊𝚍𝚖𝚒𝚗𝚜 𝚘𝚏 𝚝𝚑𝚎 𝚛𝚎𝚜𝚙𝚎𝚌𝚝𝚒𝚟𝚎 𝚐𝚛𝚘𝚞𝚙. 𝙳𝚘𝚗'𝚝 𝚖𝚒𝚜𝚞𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍.
+
+➤ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐧𝐝 𝐔𝐬𝐚𝐠𝐞:
+
+➢ /reports on/off: To Turn ON & Turn OFF reporting in your group.
+➢ /report, @admin or @admins - To report a user to the admins (𝗋𝖾𝗉𝗅𝗒 𝗍𝗈 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾).
+➢ /reports: To check the current settings."""
+
+    FORCESUB_TXT = """**♦️ READ THIS INSTRUCTION ♦️**
+
+__🗣 In Order To Get The Movie Requested By You in Our Groups, You Will Have To Join Our Main Channel First. After That, Try Accessing That Movie Again From Our Group. I'll Send You That Movie Privately 🙈__
+
+**👇 JOIN THIS CHANNEL & TRY AGAIN 👇**"""
+
+    LOG_TEXT_G = """#NewGroup
+Group = @{} (<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}"""
+
+    ZOMBIES_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙆𝙞𝙘𝙠
+
+<b>Kick incative members from group. Add me as admin with ban users permission in group.</b>
+
+<b>Commands and Usage:</b>
+• /inkick - command with required arguments and i will kick members from group.
+• /instatus - to check current status of chat member from group.
+• /inkick within_month long_time_ago - to kick users who are offline for more than 6-7 days.
+• /inkick long_time_ago - to kick members who are offline for more than a month and Deleted Accounts.
+• /del_kick - to kick deleted accounts."""
+
+    YTTHUMB_TXT = """𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝙔𝙤𝙪𝙩𝙪𝙗𝙚 𝙑𝙞𝙙𝙚𝙤 𝙏𝙝𝙪𝙢𝙗𝙣𝙖𝙞𝙡
+⭕𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+𝘛𝘺𝘱𝘦 /ytthumb 𝘈𝘯𝘥 𝘝𝘪𝘥𝘦𝘰 𝘓𝘪𝘯𝘬
+
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦
+/ytthumb https://youtu.be/yourlink"""
+
+    CREATOR_REQUIRED = """❗<b>You have To Be The Group Creator To Do That.</b>"""
+      
+    INPUT_REQUIRED = "❗ **Arguments Required**"
+      
+    KICKED = """✔️ Successfully Kicked {} Members According To The Arguments Provided."""
+      
+    START_KICK = """🚮 Removing Inactive Members This May Take A While..."""
+      
+    ADMIN_REQUIRED = """❗<b>I Am Not An Admin Here\n__Leaving This Chat, Add Me Again As Admin With Ban User Permission.</b>"""
+      
+    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
+      
+    FETCHING_INFO = """<b>Collecting Users Information...</b>"""
+      
+    STATUS = """{}\n<b>Chat Member Status</b>**\n\n```<i>Recently``` - {}\n```Within Week``` - {}\n```Within Month``` - {}\n```Long Time Ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}</i>"""
+
+    WARNS_TXT = """Here is the help for the Warns module:
+
+Keep your members in check with warnings; stop them getting out of control!
+If you're looking for automated warnings, read about the blacklist module!
+
+Admin Commands:
+- /warn <reason>: Warn a user.
+- /dwarn <reason>: Warn a user by reply, and delete their message.
+- /swarn <reason>: Silently warn a user, and delete your message.
+- /warns: See a user's warnings.
+- /rmwarn: Remove a user's latest warning.
+- /resetwarn: Reset all of a user's warnings to 0.
+- /resetallwarns: Delete all the warnings in a chat. All users return to 0 warns.
+- /warnings: Get the chat's warning settings.
+- /setwarnmode <ban/kick/mute>: Set the chat's warn mode.
+- /setwarnlimit <number>: Set the number of warnings before users are punished.
+
+Examples
+- Warn a user.
+-> /warn @user For disobeying the rules"""
+
+    GENSTR_TXT = """wait"""
+    
+    GBAN_TXT = """GBAN"""
+
+    LOCKS_TXT = """Here is the help for the Locks module:
+
+Admin only:
+× /lock <permission>: Lock Chat permission..
+× /unlock <permission>: Unlock Chat permission.
+× /locks: View Chat permission.
+× /locktypes: Check available lock types!
+
+Locks can be used to restrict a group's users.
+Locking urls will auto-delete all messages with urls, locking stickers will delete all stickers, etc.
+Locking bots will stop non-admins from adding bots to the chat.
+
+Example:
+/lock media: this locks all the media messages in the chat."""
+
+    APPROVAL_TXT = """Here is the help for the Approvals module:
+
+Sometimes, you might trust a user not to send unwanted content.
+Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+That's what approvals are for - approve of trustworthy users to allow them to send
+
+User commands:
+× /approval: Check a user's approval status in this chat.
+
+Admin Commands:
+× /approve: Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+× /unapprove: Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+× /approved: List all approved users.
+
+Owner Commands:
+× /unapproveall: Unapprove ALL users in a chat. This cannot be undone."""
+
+    BLOCKLIST_TXT = """Here is the help for the Blacklists module:
+
+User Commands:
+× /blacklists: Check all the blacklists in chat.
+
+Admin Commands:
+× /addblacklist <trigger>: Blacklists the word in the current chat.
+× /rmblacklist <trigger>: Removes the word from current Blacklisted Words in Chat.
+× /blaction <mute/kick/ban/warn/none>: Sets the action to be performed by bot when a blacklist word is detected.
+× /blacklistaction: Same as above
+
+Owner Only:
+× /remallbl: Removes all the blacklisted words from chat
+
+Note:
+The Default mode for Blacklist is none, which will just delete the messages from the chat."""
+
+    RULES_TXT = """Here is the help for the Rules module:
+
+Every chat works with different rules; this module will help make those rules clearer!
+User commands:
+× /rules: Check the current chat rules.
+Admin commands:
+× /setrules <text>: Set the rules for this chat.
+× /privaterules <yes/no/on/off>: Enable/disable whether the rules should be sent in private.
+× /resetrules: Reset the chat rules to default
+× /rulesbtn <custom text>: Sets the text of rules button.
+× /resetrulesbutton: Reset the text of rules button to default.
+× /resetrulesbtn: Same as above."""
+
+    NOTES_TXT = """Here is the help for the Notes module:
+
+Save data for future users with notes!
+Notes are great to save random tidbits of information; a phone number, a nice gif, a funny picture - anything!
+User commands:
+- /get <notename>: Get a note.
+- #notename: Same as /get.
+Admin commands:
+- /save <notename> <note text>: Save a new note called "word". Replying to a message will save that message. Even works on media!
+- /clear <notename>: Delete the associated note.
+- /notes: List all notes in the current chat.
+- /saved: Same as /notes.
+- /clearall: Delete ALL notes in a chat. This cannot be undone.
+- /privatenotes: Whether or not to send notes in PM. Will send a message with a button which users can click to get the note in PM."""
+
+    WELCOME_TXT = """Here is the help for the Welcome module:
+
+Welcome new members to your groups or say Goodbye after they leave!
+
+Admin Commands:
+× /setwelcome <reply/text>: Sets welcome text for group.
+× /welcome <yes/no/on/off>: Enables or Disables welcome setting for group.
+× /resetwelcome: Resets the welcome message to default.
+× /setgoodbye <reply/text>: Sets goodbye text for group.
+× /goodbye <yes/no/on/off>: Enables or Disables goodbye setting for group.
+× /resetgoodbye: Resets the goodbye message to default.
+× /cleanservice <yes/no/on/off>: Delete all service messages such as 'x joined the group' notification.
+× /cleanwelcome <yes/no/on/off>: Delete the old welcome message, whenever a new member joins."""
