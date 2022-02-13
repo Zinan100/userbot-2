8de0d4d0d247054c864db9e67def3d1ec5d609f5
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-13 13:54:53 +0530

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,26 @@
+class Presets(object):
+    PROCESSING = "<i>Processing..</i>"
+    CHANGE_LANG_TXT = "<i>Please select the required language.</i>"
+    DOWNLOADING = "<i>Downloading your voice..</i>"
+    CONVERTING = "<i>Converting your voice..</i>"
+    CHUNK_ERROR = "<b>Some unknown errors happened while converting your voice. Anyways, I'll send you the completed " \
+                  "texts. Inconvenience is regretted.</b> 🥵"
+    ERROR_CONVERTING = "<i>Error converting your voice..</i>"
+    ERROR_FILE = "<b>Error!</b>\n\n<i>This audio cannot be able to convert</i>"
+    LONG_FILE = "🧐 <b><u>Hmm.. Lengthy audio</u></b>\n\n<i>Let's try to convert it..</i>"
+    CHUNK_TXT = "<i>Generating the texts from Audio: {}</i>"
+    FINISHED = "<i>Finished Extracting !</i>"
+    READ_TEXT = "<i>Recognizing the text... </i>"
+    DECODED_TEXT = "<b><u>Decoded text:</u></b>\n<i>--Tap the text to copy--</i>\n\n<code>{}</code>\n\n" \
+                   "<b>Credits:</b><a href='https://github.com/m4mallu'><b> M4Mallu</b></a>"
+    DECODED_LONG_TEXT = "<b><u>Decoded text:</u></b>\n\n<code>{}</code>\n\n" \
+                        "<b>Credits:</b><a href='https://github.com/m4mallu'><b> M4Mallu</b></a>"
+    ERROR_DECODE = "<b>Error!</b>\n\n<i>This audio cannot be able to decode</i>"
+    LANGUAGE_SELECT_MSG = "Selected Language: {}"
+    TEXT_TO_VOICE = "<b>Select the accent to convert:</b>"
+    CONV_TO_VOICE = "<i>Converting your text to voice..</i>"
+    GEN_VOICE = "<i>Generating the voice..</i>"
+    VOICE_UPLOADED = "<i>Uploading the voice..Plz wait</i>"
+    VOICE_CAPTION = "<b>Credits:</b><a href='https://github.com/m4mallu'><b> M4Mallu</b></a>"
+    A2T_REPORT = "<b><u>Intimation:</u></b>\n\n<i>Audio to Text conversion</i>\nby: {}"
+    T2A_REPORT = "<b><u>Intimation:</u></b>\n\n<i>Text to Audio conversion</i>\nby: {}"
