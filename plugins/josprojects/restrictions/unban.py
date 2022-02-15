c3919aa5c8509c768e23673b8defd43133ad4500
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-15 14:41:06 +0530

    Update unban.py

--- plugins/josprojects/restrictions/unban.py
+++ plugins/josprojects/restrictions/unban.py
@@ -3,7 +3,7 @@ from plugins.josprojects.help_func.admin_check import admin_check
 from plugins.josprojects.help_func.extract_user import extract_user
 
 
-@Client.on_message(filters.command(["unban", "unmute"]))
+@Client.on_message(filters.command(["removeban", "removemute"]))
 async def un_ban_user(_, message):
     is_admin = await admin_check(message)
     if not is_admin:
