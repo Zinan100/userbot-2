9b89e768eefe27c35955ca562e3b82f02cbef852
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-16 12:47:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,9 +52,11 @@ async def start(client, message):
             InlineKeyboardButton('𝖧𝖾𝗅𝗉 💭', callback_data='hellp')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode='html'
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
