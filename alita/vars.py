e383e2770234c4cbed234016bf81f7636d7da864
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-17 19:41:05 +0530

    Create vars.py

---
+++ alita/vars.py
@@ -0,0 +1,13 @@
+from os import getcwd
+
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
+
+env_file = f"{getcwd()}/.env"
+config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
+
+
+class Config:
+    """Config class for variables."""
+
+    LOGGER = True
