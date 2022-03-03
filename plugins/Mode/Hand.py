384fc0012f74d7a2e9242b8a7f3b834a7d9a4cc0
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-03-03 13:35:14 +0530

    Update Hand.py

--- plugins/Mode/Hand.py
+++ plugins/Mode/Hand.py
@@ -2,7 +2,7 @@ import pyrogram
 from pyrogram import Client, filters
 from pyrogram.types import User, Message
 import requests
-Import os
+import os
 
 @Client.on_message(filters.command('hand'))
 async def text(bot, message):
