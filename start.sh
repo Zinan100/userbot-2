0b3042d9509d55e27f917a73397c7e5783953cbb
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-03-06 13:47:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://git.heroku.com/0 /0
+  git clone https://git.heroku.com/evamariatgokdabot.git /evamariatgokdabot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /0
+  git clone $UPSTREAM_REPO /evamariatgokdabot
 fi
-cd /0
+cd /evamariatgokdabot
 pip freeze > requirements.txt
 echo "Starting Bot...."
 python3 bot.py
