ef2ca1f74f7d0cd8784096b0ed126ed8cd3659e6
Alif <90495044+Ashifmu@users.noreply.github.com>
2022-03-25 15:34:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://git.heroku.com/evamariatgokdabot.git /evamariatgokdabot
+  git clone https://git.heroku.com/clifford-bot-v4.git /clifford-bot-v4
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /evamariatgokdabot
+  git clone $UPSTREAM_REPO /clifford-bot-v4
 fi
-cd /evamariatgokdabot
+cd /clifford-bot-v4
 pip freeze > requirements.txt
 echo "Starting Bot...."
 python3 bot.py
